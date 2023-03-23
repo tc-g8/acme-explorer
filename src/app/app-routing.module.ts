bb14430249f0f2552001c26b816fda9794535a2c
alemunara <alemunara@alum.us.es>
2023-03-23 17:40:23 +0100

    feat: added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/security/register/register.component';
+import { LoginComponent } from './components/security/login/login.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegisterComponent }, // Primera vista (por defecto)
+  { path: '**', component: NotFoundComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
