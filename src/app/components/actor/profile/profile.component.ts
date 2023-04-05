6cba226fab64233c40acc4b2c43d7bd6df722feb
Mario Ruano <mrf1989@hotmail.com>
2023-04-05 19:39:37 +0200

    feat: add data to actor profile

--- src/app/components/actor/profile/profile.component.ts
+++ src/app/components/actor/profile/profile.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Actor } from 'src/app/models/actor.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  actor: Actor;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.actor = new Actor();
   }
 
+  ngOnInit(): void {
+    this.actor = this.authService.getCurrentActor()!;
+  }
 }
