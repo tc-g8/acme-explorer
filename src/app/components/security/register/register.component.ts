071a7ea1ceb021b2712302238e0a6387050466a3
Mario Ruano <mruano@us.es>
2023-04-12 13:04:06 +0200

    fix messages in signup form and restyle header

--- src/app/components/security/register/register.component.ts
+++ src/app/components/security/register/register.component.ts
@@ -2,46 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   registrationForm: FormGroup;
   errorMessage: string = '';
   successMessage: string = '';
 
-  constructor(private authService: AuthService, private fb: FormBuilder) {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private messageService: MessageService
+  ) {
     this.registrationForm = this.fb.group({
       name: [''],
       surname: [''],
       email: [''],
       phone: [''],
       address: [''],
-      password: ['']
+      password: [''],
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onRegister() {
-    this.authService.registerUser(this.registrationForm.value)
-      .then(res => {
+    this.authService.registerUser(this.registrationForm.value).then(
+      (res) => {
         this.errorMessage = '';
         this.successMessage = 'Registration successful';
-      }, err => {
+        this.messageService.notifyMessage(
+          this.successMessage,
+          'alert alert-success'
+        );
+      },
+      (err) => {
         if (err.status === 422) {
-          console.log('There are some errors in the data introduced');
           this.errorMessage = 'There are some errors in the data introduced';
+          console.log(this.errorMessage);
         } else {
           console.log(err);
-          this.errorMessage = 'There was an error during the registration process';
+          this.errorMessage =
+            'There was an error during the registration process';
         }
+        this.messageService.notifyMessage(
+          this.errorMessage,
+          'alert alert-danger'
+        );
         this.successMessage = '';
-      });
+      }
+    );
   }
 }
