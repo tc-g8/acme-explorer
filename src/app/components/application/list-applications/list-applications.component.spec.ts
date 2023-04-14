234531763dba363e3ee14da7aa0a50429b4e638e
alemunara <alemunara@alum.us.es>
2023-04-14 23:36:39 +0200

    test: added application list tests

--- src/app/components/application/list-applications/list-applications.component.spec.ts
+++ src/app/components/application/list-applications/list-applications.component.spec.ts
@@ -1,23 +1,88 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { ApplicationStatus } from 'src/app/enums/application.enum';
+import { Application } from 'src/app/models/application.model';
+import { ApplicationService } from 'src/app/services/application.service';
+import { environment } from 'src/environments/environment';
+import { ActivatedRouteStub } from '../../shared/activatedroute-stub';
 
-import { ListExplorerApplicationsComponent } from './list-applications.component';
+import { ListApplicationsComponent } from './list-applications.component';
 
-describe('ListExplorerApplicationsComponent', () => {
-  let component: ListExplorerApplicationsComponent;
-  let fixture: ComponentFixture<ListExplorerApplicationsComponent>;
+describe('ListApplicationsComponent', () => {
+  let component: ListApplicationsComponent;
+  let fixture: ComponentFixture<ListApplicationsComponent>;
+  let mockActivatedRoute: ActivatedRouteStub;
+  let getApplicationsByTripIdSpy: Application[];
+  let testApplicationList: Application[];
+  let testApplication1: Application;
+  let testApplication2: Application;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListExplorerApplicationsComponent ]
-    })
-    .compileComponents();
+    mockActivatedRoute = new ActivatedRouteStub();
+    mockActivatedRoute.testParams = { tripId: '64344f7965cca599602c91a5' };
+    testApplicationList = [];
 
-    fixture = TestBed.createComponent(ListExplorerApplicationsComponent);
+    testApplication1 = new Application();
+    testApplication1.requestDate = new Date('2023-04-13');
+    testApplication1.status = ApplicationStatus.ACCEPTED;
+    testApplication1.comment =
+      'Ad sunt sunt consectetur veniam ut. Fugiat sunt aute fugiat ut adipisicing ipsum eiusmod. Laboris ut aliquip anim cillum nostrud proident est fugiat anim esse.';
+    testApplication1.rejectedReason = undefined;
+    testApplication1.trip_id = '64344f7965cca599602c91a5';
+    testApplication1.paidAt = new Date('2023-04-14');
+
+    testApplication2 = new Application();
+    testApplication2.requestDate = new Date('2023-04-12');
+    testApplication2.status = ApplicationStatus.ACCEPTED;
+    testApplication2.comment =
+      'Ad sunt sunt consectetur veniam ut. Fugiat sunt aute fugiat ut adipisicing ipsum eiusmod. Laboris ut aliquip anim cillum nostrud proident est fugiat anim esse.';
+    testApplication2.rejectedReason = undefined;
+    testApplication2.trip_id = '64344f7965cca599602c91a5';
+    testApplication2.paidAt = new Date('2023-04-13');
+
+    let applicationsByTripIdSpy = jasmine.createSpyObj('ApplicationService', [
+      'getApplicationsByTripId',
+    ]);
+    getApplicationsByTripIdSpy =
+      applicationsByTripIdSpy.getApplicationsByTripId.and.returnValue(
+        of(testApplicationList)
+      );
+
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        RouterModule,
+      ],
+      declarations: [ListApplicationsComponent],
+      providers: [
+        { provide: ApplicationService, useValue: applicationsByTripIdSpy },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        AngularFireAuth,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ListApplicationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should list two applications', () => {
+    testApplicationList = [testApplication1, testApplication2];
+    fixture.detectChanges();
+    expect(component.applications.length === 2);
+  });
+
+  it('should not list applications', () => {
+    fixture.detectChanges();
+    expect(component.applications.length === 0);
   });
 });
--- src/app/services/application.service.spec.ts
+++ src/app/services/application.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 import { ApplicationService } from './application.service';
 
@@ -6,7 +9,12 @@ describe('ApplicationService', () => {
   let service: ApplicationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        HttpClientModule,
+      ],
+    });
     service = TestBed.inject(ApplicationService);
   });
 
