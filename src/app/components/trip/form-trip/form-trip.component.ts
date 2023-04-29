94a2c3a8458d7937378030f58e1454f686cb4ada
Mario Ruano <mrf1989@hotmail.com>
2023-04-29 16:29:13 +0200

    feat: implements encode image system in create trip form

--- src/app/components/trip/form-trip/form-trip.component.ts
+++ src/app/components/trip/form-trip/form-trip.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Stage } from 'src/app/models/stage.model';
 
 @Component({
   selector: 'app-form-trip',
   templateUrl: './form-trip.component.html',
-  styleUrls: ['./form-trip.component.css']
+  styleUrls: ['./form-trip.component.css'],
 })
 export class FormTripComponent implements OnInit {
+  tripForm: FormGroup;
+  imagesCollection: string[];
+  stagesCollection: Stage[];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.tripForm = this.fb.group({
+      manager_id: [''],
+      title: [''],
+      description: [''],
+      startDate: [''],
+      endDate: [''],
+      requirements: [''],
+      images: [''],
+    });
 
-  ngOnInit(): void {
+    this.stagesCollection = [];
+    this.imagesCollection = [];
   }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {}
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.convertBase64(file).then((encodedImage) => {
+        this.imagesCollection.push(encodedImage as string);
+      });
+    }
+  }
+
+  removeImage(image: string) {
+    this.imagesCollection = this.imagesCollection.filter(
+      (i: string) => i != image
+    );
+  }
+
+  private convertBase64(file: File) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
 }
